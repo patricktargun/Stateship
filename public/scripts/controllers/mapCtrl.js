987f73c8c43dcad4c9148414db2a365a222ecf93
Jess Haddow <jess.haddow@gmail.com>
2014-11-10 17:27:52 -0700

    Updates imgs and brings data to stateCtrl

--- public/scripts/controllers/mapCtrl.js
+++ public/scripts/controllers/mapCtrl.js
@@ -1,19 +1,17 @@
-(function(){
-	angular
-		.module('stateship')
-		.controller('MapCtrl', MapCtrl);
+(function() {
+    angular
+        .module('stateship')
+        .controller('MapCtrl', MapCtrl);
 
-		function MapCtrl(StateService, $location, $scope){
-			var vm = this;
-			vm.setCurrentState = setCurrentState;
+    function MapCtrl(StateService, $location, $scope) {
+        var vm = this;
+        vm.setCurrentState = setCurrentState;
 
-			function setCurrentState(state){
-				console.log(state);
+        function setCurrentState(state) {
+            console.log(state);
+            $location.path('/state/' + state);
+            $scope.$apply();
 
-				
-				$location.path('/state/' + state);
-				$scope.$apply();
-				
-			}
-		}
+        }
+    }
 })();
