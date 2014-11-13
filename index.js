c25a95e71be75c7bd6dd160427c34080ef73d4bc
Daniel Kesler <danielckesler@gmail.com>
2014-11-13 15:25:59 -0700

    Did some more CORS stuff

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ var express = require('express'),
 var app = express(),
     port = 9901;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "www.google.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-//
-var middle = function(req, res, next){
-    if(req.headers.origin!== 'http://localhost:9901'){
-        res.status(401).json("Email requests must be made from stateship.org");
+var CORS = function(req, res, next){
+    if(req.headers.origin === 'http://localhost:9901' || req.headers.origin === 'http://stateship.org'){
+        next();
     }
     else {
-        next();
+        res.status(401).json("Email requests must be made from stateship.org");
     }
 };
 
@@ -27,11 +20,11 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 //End Point to hit for dirty politician info.
-app.post('/api/representatives', middle, civic.getReps);
-app.post('/api/voterInfo', civic.getVoterInfo);
+app.post('/api/representatives', CORS, civic.getReps);
+app.post('/api/voterInfo', CORS, civic.getVoterInfo);
 
 //End Point to hit for sending Emails. Uses Mandrill
-app.post('/api/sendemail', middle, MandrillCtrl.sendEmail);
+app.post('/api/sendemail', CORS, MandrillCtrl.sendEmail);
 
 
 app.listen(port, function() {
