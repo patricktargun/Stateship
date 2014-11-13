fc91b64e6ee85b3b85b9b4a036cf8a5f6cdb4d62
Daniel Kesler <danielckesler@gmail.com>
2014-11-13 14:59:19 -0700

    fixed CORS issue with origins and the email endpoint

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ var express = require('express'),
 var app = express(),
     port = 9901;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "www.google.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//
+var middle = function(req, res, next){
+    if(req.headers.origin!== 'http://localhost:9901'){
+        res.status(401).json("Email requests must be made from stateship.org");
+    }
+    else {
+        next();
+    }
+};
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 //End Point to hit for dirty politician info.
-app.post('/api/representatives', civic.getReps);
+app.post('/api/representatives', middle, civic.getReps);
 app.post('/api/voterInfo', civic.getVoterInfo);
 
 //End Point to hit for sending Emails. Uses Mandrill
-app.post('/api/sendemail', MandrillCtrl.sendEmail);
+app.post('/api/sendemail', middle, MandrillCtrl.sendEmail);
 
 
 app.listen(port, function() {
