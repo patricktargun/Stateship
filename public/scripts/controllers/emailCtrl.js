ef9bf0732fbc89df4b051202a9b93714a7de584b
Jess Haddow <jess.haddow@gmail.com>
2014-11-14 11:32:14 -0700

    nzSweetAlert works in emailCtrl

--- public/scripts/controllers/emailCtrl.js
+++ public/scripts/controllers/emailCtrl.js
@@ -3,7 +3,7 @@
         .module('stateship')
         .controller('EmailCtrl', EmailCtrl);
 
-    function EmailCtrl(currentRep, EmailService) {
+    function EmailCtrl(currentRep, EmailService, nzSwal) {
         var vm = this;
         console.log("got Here");
         vm.sendEmail = sendEmail;
@@ -20,9 +20,24 @@
                 });
         }
 
-        function resetForm() {
-            for (var key in email)
-                email[key] = "";
+        function resetForm(email) {
+            nzSwal({
+                    title: "Are you sure?",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: '#DD4B39',
+                    confirmButtonText: "Yes, do it!",
+                    closeOnConfirm: false
+                })
+                .then(function() {
+                    nzSwal('You did it!');
+                    for (var key in email)
+                        email[key] = "";
+                })
+                .catch(function() {
+                    nzSwal('Cancelled :)');
+                });
+            
         }
     }
 })();
