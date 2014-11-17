1f287942914bb18da28b52f167968bf03b55b949
PJ Targun <pjtargun@gmail.com>
2014-11-17 11:13:25 -0700

    Val Stuff

--- public/scripts/controllers/emailCtrl.js
+++ public/scripts/controllers/emailCtrl.js
@@ -3,7 +3,7 @@
         .module('stateship')
         .controller('EmailCtrl', EmailCtrl);
 
-    function EmailCtrl(currentRep, EmailService) {
+    function EmailCtrl(currentRep, EmailService, $scope) {
         var vm = this;
         console.log("got Here");
         vm.sendEmail = sendEmail;
@@ -12,12 +12,18 @@
 
 
         function sendEmail(email) {
+            $scope.$broadcast('show-errors-check-validity');
+            if (!$scope.searchReps.$valid) {
+                return; 
+            }
+            else if ($scope.searchReps.$valid) {
             email.to = currentRep.emails[0];
             EmailService.sendEmail(email)
                 .then(function() {
                     for (var key in email)
                         email[key] = "";
                 });
+            }
         }
 
         function resetForm() {
