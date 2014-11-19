0b41f0940920a09a2cad3d86d8fd5005a8d092b4
Daniel Kesler <danielckesler@gmail.com>
2014-11-19 12:10:20 -0700

    IE detection

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -83,31 +83,6 @@
 				$location.path("/state/"+vm.address.state.abbreviation.toLowerCase()+"/"+vm.address.street+','+vm.address.city);
 			}
 		}
-		function browser(){
-			if(navigator.sayswho= (function(){
-				var ua= navigator.userAgent, tem,
-					M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-				if(/trident/i.test(M[1])){
-					tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-					return 'IE '+(tem[1] || '');
-				}
-				if(M[1]=== 'Chrome'){
-					tem= ua.match(/\bOPR\/(\d+)/)
-					if(tem!= null) return 'Opera '+tem[1];
-				}
-				M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-				if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-					return M.join(' ');
-					})().indexOf("IE") !== -1)
-				{
-					nzSwal({
-					title: "Stateship.org is not optimized to run well in Internet Explorer. \n Sorry for the inconvenience.",
-					confirmButtonColor: "#4091CD",
-					confirmButtonText: "Okay"
-					});
-				}
-		}
-		browser();
 	}
 	
 })();
